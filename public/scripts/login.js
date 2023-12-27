81a776a76e036e2ef3a6b184df3dbc332bc797ec
RANJITH KUMAR <59201846+RANJITH1799@users.noreply.github.com>
2023-12-27 22:48:44 +0530

    Uploaded JS Files

---
+++ public/scripts/login.js
@@ -0,0 +1,38 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+  event.preventDefault();
+  
+  var usrnameoremail = document.getElementById('usernameOrEmail').value;
+  var psword = document.getElementById('password').value;
+
+  fetch('/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ username_or_email: usrnameoremail, password: psword })
+  })
+  .then(response => {
+    if (response.status === 401) {
+      return { authenticated: false };
+    }
+    if (response.ok) {
+      return response.json();
+    }
+    throw new Error('Network response was not ok.');
+  })
+  .then(data => {
+    console.log(data);
+    if (data.authenticated) {
+      if (data.isAdmin) {
+        window.location.href = '/sample.html';
+      } else {
+        window.location.href = '/user.html';
+      }
+    } else {
+      alert('Invalid Username or Password!');
+    }
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });  
+});
