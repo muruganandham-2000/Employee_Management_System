809f5dbb4809ddb6731095f4bc804117cfb6895d
Muruganandham <muruganandham5080@gmail.com>
2024-01-04 00:22:55 +0530

    to-do list backend done

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const session = require('express-session');
 const crypto = require('crypto');
 const loginRoutes = require('./node_script/login_routes');
 const dashboard = require('./node_script/admin_dashboard');
+const tasks = require('./node_script/tasks_routes');
 const userRoutes = require('./node_script/user_routes');
 
 const generateRandomString = (length) => {
@@ -26,7 +27,8 @@ app.use(session({
 app.use(express.static('public'));
 
 app.use(loginRoutes);
-app.use('/admin', dashboard); 
+app.use('/admin', dashboard);
+app.use('/admin', tasks); 
 app.use('/admin', userRoutes);
 
 const PORT = 3000;
